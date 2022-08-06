e6ba27f0fa8c55a5e3a2bec6ecdce72795f93547
Mintae <alsxo99@snu.ac.kr>
2022-08-06 23:38:27 +0900

    #4 객체를 Table에 맞추어 Modeling (연관관계 X)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,20 +6,37 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator
-        (name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id
-    @GeneratedValue
-            (strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "USERNAME")
     private String username;
+
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
 
     public Member() {}
 
