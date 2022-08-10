4ace68012ad0471d74d9bcb7a151f1895c47c4d1
Mintae <alsxo99@snu.ac.kr>
2022-08-11 01:28:49 +0900

    #8 다대다(N:M) 연관관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,20 +12,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-
-            Locker locker = new Locker();
-            em.persist(locker);
-
-            Member member = new Member();
-            member.setUsername("A");
-            member.setTeam(team);
-            member.setLocker(locker);
-
-            em.persist(member);
-
 
             tx.commit();
         } catch (Exception e) {
