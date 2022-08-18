1251fa7b5ee43c1e336ef8d66d11bdd746a419dc
Mintae <alsxo99@snu.ac.kr>
2022-08-18 19:42:01 +0900

    #2 즉시 로딩과 지연 로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,23 +13,30 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member = new Member();
             member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
 
             em.flush();
             em.clear();
 
 //            Member findMember = em.find(Member.class, member.getId());
-            Member reference = em.getReference(Member.class, member.getId());
+//            Team findTeam = findMember.getTeam();
 
-            System.out.println(reference.getUsername());
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
 
-            Member member2 = em.find(Member.class, member.getId());
 
-            System.out.println(member2.getClass());
-            System.out.println(member2 == reference);
-
+//            System.out.println(findTeam.getClass());
+//
+//            System.out.println("==============");
+//            System.out.println(findTeam.getName());
+//            System.out.println("==============");
 
             tx.commit();
         } catch (Exception e) {
